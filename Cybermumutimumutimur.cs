324d23aef0d3965d067cb23c1f467b6fd1d07566
Amirangun <120313235+Amirangun@users.noreply.github.com>
2022-12-11 19:05:50 +0600

    A9A9A9
    LOOOOOL

---
+++ Cybermumutimumutimur.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cybermumutimumutimur : MonoBehaviour
+{
+    public rigiddody rb;
+    public float right;
+    public float left;
+    public float speed = 5;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if Input(GetKey.(KeyCode.A));
+            transform.translate speed * left * Time.deltaTime
+    }
+}
